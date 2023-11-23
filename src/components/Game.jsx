55a659d5506219585a6e5f635354cd53618f182a
Flávia Reis <flaviarster@gmail.com>
2023-11-23 12:04:38 -0300

    Fiz alterações para que espaços nas respostas do usuário não sejam considerados erro

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -22,49 +22,52 @@ export function Game({ moviesData }) {
     const remainingHints = totalHints - currentHintIndex - 1
   
     const handleGuess = (userGuess) => {
-      console.log('Handling guess:', userGuess)
+      console.log('Handling guess:', userGuess);
       if (!hasGuessed) {
         if (!userGuess.trim()) {
-          alert('Por favor, dê um palpite!')
-          return
+          alert('Please make a guess!');
+          return;
         }
-  
-        if (userGuess.toLowerCase() === currentMovie.title.toLowerCase()) {
-          const pointsEarned = 500 - currentHintIndex * 100
-          setScore(score + pointsEarned)
-  
+    
+        const cleanUserGuess = userGuess.trim().toLowerCase().replace(/[^\w\s]/gi, '');
+        const cleanMovieTitle = currentMovie.title.trim().toLowerCase().replace(/[^\w\s]/gi, '');
+    
+        if (cleanUserGuess === cleanMovieTitle) {
+          const pointsEarned = 500 - currentHintIndex * 100;
+          setScore(score + pointsEarned);
+    
           if (currentMovieIndex < moviesData.length - 1) {
-            alert('Parabéns! Você acertou!')
+            alert('Parabéns! Você acertou!');
             setTimeout(() => {
-              setCurrentMovieIndex(currentMovieIndex + 1)
-              setCurrentHintIndex(0)
-              setHasGuessed(false)
-              setShowGuessBox(true)
-              setShowHints(true)
-              setGuess('')
-            }, 500)
+              setCurrentMovieIndex(currentMovieIndex + 1);
+              setCurrentHintIndex(0);
+              setHasGuessed(false);
+              setShowGuessBox(true);
+              setShowHints(true);
+              setGuess('');
+            }, 500);
           } else {
-            setCongratulationsMessage('You have completed the game!')
-            setShowOptions(true)
+            setCongratulationsMessage('Você finalizou o jogo!');
+            setShowOptions(true);
           }
         } else {
-          setLives(lives - 1)
-  
+          setLives(lives - 1);
+    
           if (lives === 1) {
-            alert('Você perdeu todas as vidas!')
-            setShowOptions(true)
+            alert('Você perdeu todas as vidas!');
+            setShowOptions(true);
           } else {
-            alert('Você perdeu uma vida!')
-            setShowOptions(true)
+            alert('Você perdeu uma vida!');
+            setShowOptions(true);
           }
-  
-          setGuess('')
-          setHasGuessed(true)
-          setShowGuessBox(false)
-          setShowHints(false)
+    
+          setGuess('');
+          setHasGuessed(true);
+          setShowGuessBox(false);
+          setShowHints(false);
         }
       }
-    }
+    };
   
     const handleOptionClick = (option) => {
       console.log('Option clicked:', option)
@@ -85,7 +88,7 @@ export function Game({ moviesData }) {
           setHasGuessed(false)
           setShowHints(true)
         } else {
-          setCongratulationsMessage('Congratulations! You finished the game!')
+          setCongratulationsMessage('Parabéns! Você finalizou o jogo!')
           setShowOptions(true)
         }
       }
