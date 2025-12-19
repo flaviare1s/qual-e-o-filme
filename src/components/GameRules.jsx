7d92f1f881f68bd04da25f183536d44c16d1899f
flaviare1s <flaviarster@gmail.com>
2025-12-19 08:28:25 -0300

    Ajusta estilos

--- src/components/GameRules.jsx
+++ src/components/GameRules.jsx
@@ -5,13 +5,15 @@ export const GameRules = () => {
     <div className='rules-wrapper'>
       <div className='rules'>
           <h2>Regras do Jogo</h2>
-              <p>Tente adivinhar o nome do filme.</p>
-              <p>Você tem direito a cinco dicas.</p>
-              <p>A cada palpite errado, você perde uma vida.</p>
-              <p>Cuidado, você só tem dez vidas.</p>
-              <p>Você recebe mais pontos se usar menos dicas.</p>
-              <p>Escreva o nome dos filmes corretamente, colocando a acentuação.</p>
-              <p>Boa sorte e vamos começar!</p>
+              <ul>
+                <li>Tente adivinhar o nome do filme.</li>
+                <li>Você tem direito a cinco dicas.</li>
+                <li>A cada palpite errado, você perde uma vida.</li>
+                <li>Cuidado, você só tem dez vidas.</li>
+                <li>Você recebe mais pontos se usar menos dicas.</li>
+                <li>Escreva o nome dos filmes corretamente, colocando a acentuação.</li>
+                <li>Boa sorte e vamos começar!</li>
+              </ul>
         </div>
     </div>
   )
